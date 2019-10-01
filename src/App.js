e8d2665fbc743a80bde7311a7003f88e5cd9deaf
ArturVargas <arturvargas9226@gmail.com>
2019-10-01 13:18:39 -0500

    [add]Pages & Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// Components
+import Navbar from './Components/Navbar';
+
+// Pages
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const Home = () => (
+  <div className="homePage">
+    <div className="directoryMenu">
+      <div className="menu-item">
+        <div className="content">
+          <h1> HATS</h1>
+          <span>Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>   
+);
+
+export default Home;
